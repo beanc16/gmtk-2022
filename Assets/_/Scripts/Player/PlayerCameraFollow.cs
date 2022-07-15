00fe6d7eae99a0d4383913bdcc27411d239b928e
Philip <Meliroon@gmail.com>
2022-07-15 14:16:03 -0700

    Basic player movement using the new unity movement system.

---
+++ Assets/_/Scripts/Player/PlayerCameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace _.Scripts.Player
+{
+    public class PlayerCameraFollow : MonoBehaviour
+    {
+        private Camera mainCamera;
+
+        private void Awake()
+        {
+            mainCamera = Camera.main;
+        }
+
+        private void Update()
+        {
+            var pos = transform.position;
+            var mainCameraTransform = mainCamera.transform;
+            pos.z = mainCameraTransform.position.z; 
+            mainCameraTransform.position = pos;
+        }
+    }
+}
