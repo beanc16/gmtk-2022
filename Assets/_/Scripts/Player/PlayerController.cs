84a154568227485bcd83fde8c07e6e28888407a9
Philip <Meliroon@gmail.com>
2022-07-15 18:01:23 -0700

    Enabling the old input system and settings things up so it can use it.

--- Assets/_/Scripts/Player/PlayerController.cs
+++ Assets/_/Scripts/Player/PlayerController.cs
@@ -10,6 +10,7 @@ namespace _.Scripts.Player
         [SerializeField] private float playerSpeed;
         [SerializeField] private AttackSystem.AttackSystem attackSystem;
         [SerializeField] private Rigidbody2D playerBody2d;
+        [SerializeField] private bool useNewInputSystem;
 
         private Vector2 currentMovement2d;
 
@@ -20,16 +21,52 @@ namespace _.Scripts.Player
 
         private void OnMovement(InputValue value)
         {
-            currentMovement2d = value.Get<Vector2>() * playerSpeed;
+            if (useNewInputSystem)
+            {
+                currentMovement2d = value.Get<Vector2>() * playerSpeed;
+            }
         }
 
         private void OnAttack(InputValue value)
         {
-            attackSystem.Attack(0);
+            if (useNewInputSystem)
+            {
+                attackSystem.Attack(0);
+            }
         }
 
         private void Update()
         {
+            if (useNewInputSystem == false)
+            {
+                currentMovement2d = Vector2.zero;
+
+                if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                {
+                    currentMovement2d.y = playerSpeed;
+                }
+
+                if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                {
+                    currentMovement2d.y = -playerSpeed;
+                }
+
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                {
+                    currentMovement2d.x = -playerSpeed;
+                }
+
+                if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                {
+                    currentMovement2d.x = playerSpeed;
+                }
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    attackSystem.Attack(0);
+                }
+            }
+
             playerBody2d.MovePosition(playerBody2d.position + currentMovement2d);
         }
     }
