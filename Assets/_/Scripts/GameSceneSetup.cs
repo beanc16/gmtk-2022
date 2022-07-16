7afe53d7ae46911998c8e81a887de2cde8746e9f
Philip <Meliroon@gmail.com>
2022-07-16 12:02:30 -0700

    Actually loading the level

--- Assets/_/Scripts/GameSceneSetup.cs
+++ Assets/_/Scripts/GameSceneSetup.cs
@@ -7,16 +7,14 @@ namespace _.Scripts
     public class GameSceneSetup : MonoBehaviour
     {
         [SerializeField] private GameController gameController;
-        //public WaveSpawner WorldPrefab;
-        public GameObject PlayerPrefab;
+        public GameObject WorldPrefab;
         public GameObject OverlayPrefab;
 
         public void Awake()
         {
             gameController.CurrentScore = 0;
             
-            //var world = Instantiate(WorldPrefab);
-            //world.SpawnPlayer(PlayerPrefab);
+            var world = Instantiate(WorldPrefab);
             Instantiate(OverlayPrefab);
         }
     }
