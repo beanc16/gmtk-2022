a6dd5eccfb72089c2d54169c754cdd9c00e29941
JohnH <Jghauge@gmail.com>
2022-07-16 22:09:56 +0200

    character animator and flipper

--- Assets/_/Scripts/Player/PlayerAnimationController.cs
+++ Assets/_/Scripts/Player/PlayerAnimationController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,5 +6,23 @@ using UnityEngine;
 public class PlayerAnimationController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rigidbody2D;
-    //[SerializeField] private 
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer renderer;
+    private readonly int _running = Animator.StringToHash("Running");
+
+    private void Update()
+    {
+        var velocity = rigidbody2D.velocity;
+
+        if (velocity.x > 0f)
+        {
+            renderer.flipX = false;
+        }
+        else if (velocity.x < 0f)
+        {
+            renderer.flipX = true;
+        }
+
+        animator.SetBool(_running, velocity != Vector2.zero);
+    }
 }
