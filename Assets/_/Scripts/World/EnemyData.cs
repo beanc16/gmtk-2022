901225119862517e3df4d6eae173951f95a72d16
Philip <Meliroon@gmail.com>
2022-07-16 17:31:44 -0700

    Adding in the idea of a melee enemy for if we want to use it.

--- Assets/_/Scripts/World/EnemyData.cs
+++ Assets/_/Scripts/World/EnemyData.cs
@@ -6,6 +6,7 @@ namespace _.Scripts.World
     [CreateAssetMenu(fileName = "EnemyData", menuName = "GMTK2022/EnemyData", order = 0)]
     public class EnemyData : ScriptableObject
     {
+        public bool HasRangeAttack;
         public float EnemySpeedMin;
         public float EnemySpeedMax;
         public float EnemyHp;
